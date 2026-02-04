ed14c4234b7842a4771c09fe930404124f394702
johnom-commits <johnom2012@yandex.ru>
2026-02-04 22:50:10 +0300

    Оптимизация и профилирование Gradle [#505354]

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,3 +105,26 @@ tasks.register("checkJarSize") {
 tasks.named("jar").configure {
     finalizedBy("checkJarSize")
 }
+
+tasks.register<Zip>("archiveResources") {
+    group = "custom optimization"
+    description = "Archives the resources folder into a ZIP file"
+
+    val inputDir = file("src/main/resources")
+    val outputDir = layout.buildDirectory.dir("archives")
+
+    inputs.dir(inputDir) // Входные данные для инкрементальной сборки
+    outputs.file(outputDir.map { it.file("resources.zip") }) // Выходной файл
+
+    from(inputDir)
+    destinationDirectory.set(outputDir)
+    archiveFileName.set("resources.zip")
+
+    doLast {
+        println("Resources archived successfully at ${outputDir.get().asFile.absolutePath}")
+    }
+}
+
+tasks.named("build") {
+    dependsOn("archiveResources")
+}
